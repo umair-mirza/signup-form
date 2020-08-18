46547b4a2ef7c6088e0d8fc8671357c26707b80c
Umair Mirza <umair_hp@hotmail.com>
2020-08-18 23:31:04 +0500

    Updates

---
+++ precache-manifest.55645bcd6976c7c60776d504b4bf69a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faf87550fc559ff4a3ccbf1bd97c86c",
+    "url": "/signup-form/index.html"
+  },
+  {
+    "revision": "79b11caf100ca01957c9",
+    "url": "/signup-form/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ca0a4449cc0f02546241",
+    "url": "/signup-form/static/css/main.fbe881aa.chunk.css"
+  },
+  {
+    "revision": "79b11caf100ca01957c9",
+    "url": "/signup-form/static/js/2.9eadc36a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/signup-form/static/js/2.9eadc36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0a4449cc0f02546241",
+    "url": "/signup-form/static/js/main.1bb51afb.chunk.js"
+  },
+  {
+    "revision": "ed4238339331b8a0344a",
+    "url": "/signup-form/static/js/runtime-main.a81a1219.js"
+  }
+]);
