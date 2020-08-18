d4e32bc18ed23122f20a5ec3d82abe2106bb6768
Umair Mirza <umair_hp@hotmail.com>
2020-08-18 23:37:49 +0500

    final deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = 
+    {
+      campaignUuid: "46aa3270-d2ee-11ea-a9f0-e9a68ccff42a",
+      data: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: ''
+      }
+    }
+  }
+
+  submit = (e) => {
+    
+    e.preventDefault();
+    fetch('https://api.raisely.com/v3/signup', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(!data.errors) {
+          alert("Congratulations! User has been created")
+        }
+        else {
+          alert("Email already registered. Please try another one!")
+        }
+      })
+  }
+
+  handleChange = (e) => {
+    this.setState({data: {...this.state.data, [e.target.name]: e.target.value}})
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="signup-form">
+          <form onSubmit={this.submit}>
+          <h2>Sign Up</h2>
+          <p>Please fill in this form to create an account!</p>
+              <div className="form-group">
+            <div className="row">
+              <div className="col"><input type="text" 
+              className="form-control" 
+              name="firstName" 
+              placeholder="First Name" 
+              onChange={this.handleChange}
+              value={this.state.data.firstName} 
+              required="required"
+               /></div>
+              <div className="col"><input type="text" 
+              className="form-control" 
+              name="lastName" 
+              placeholder="Last Name" 
+              onChange={this.handleChange}
+              value={this.state.data.lastName} 
+              required="required"
+               /></div>
+            </div>        	
+              </div>
+              <div className="form-group">
+                <input type="email" 
+                className="form-control" 
+                name="email" 
+                placeholder="Email" 
+                onChange={this.handleChange}
+                value={this.state.data.email} 
+                required="required" 
+                />
+              </div>
+          <div className="form-group">
+                  <input type="password" 
+                  className="form-control" 
+                  name="password" 
+                  placeholder="Password" 
+                  onChange={this.handleChange}
+                  value={this.state.data.password} 
+                  required="required" 
+                  />
+              </div>       
+          <div className="form-group">
+                  <button type="submit" className="btn btn-primary btn-lg">Sign Up</button>
+              </div>
+          </form>
+      </div>
+      </div>
   );
+  }
 }
 
 export default App;
